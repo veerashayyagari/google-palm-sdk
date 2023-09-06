26580df2d29f4dc24f136790f3fb45e607eab92e
Veerash Ayyagari <veerash@veerashayyagari.com>
2023-09-05 18:52:58 -0700

    Base project template commit

---
+++ src/LLMSharp.Google.Palm/GooglePalmClient.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LLMSharp.Google.Palm
+{
+    public class GooglePalmClient
+    {
+
+    }
+}
---
+++ tests/LLMSharp.Google.Palm.Tests/GooglePalmClientTests.cs
@@ -0,0 +1,11 @@
+namespace LLMSharp.Google.Palm.Tests
+{
+    [TestClass]
+    public class GooglePalmClientTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
